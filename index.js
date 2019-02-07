f6c5ce067078f3f34dd5419c727a1f7dbaaca00c
Stacy Adam <Stacy@portalmedia.com>
2019-02-06 21:26:21 -0600

    js is synchronous

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const { sequelize } = require('./models');
-
 if (process.env.NODE_ENV !== 'production') {
 	require('dotenv').config();
 }
+
+const { sequelize } = require('./models');
 
 const app = require('./app');
 
 sequelize.sync().then(() => {
 	console.log('DB connection successful');
 	app.listen(process.env.PORT, () => {
-		console.log(`Example app listening on port ${process.env.PORT}!`);
+		console.log(`Numerology App running on port ${process.env.PORT}!`);
 	});
 });
--- models/index.js
+++ models/index.js
@@ -1,17 +1,12 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(
-	'd8kgul66jsei7o',
-	'mtjsjhjbsojzsw',
-	'674384c6a8981eaeb7c3b13d535cdc86d46d5d9fceb9332681e1e295acf9566a',
-	{
-		host: 'ec2-23-21-128-35.compute-1.amazonaws.com',
-		dialect: 'postgres',
-		dialectOptions: {
-			ssl: true
-		}
+const sequelize = new Sequelize(process.env.DB, process.env.DB_USER, process.env.DB_PASSWORD, {
+	host: 'ec2-23-21-128-35.compute-1.amazonaws.com',
+	dialect: 'postgres',
+	dialectOptions: {
+		ssl: true
 	}
-);
+});
 
 const User = sequelize.import('./User');
 
